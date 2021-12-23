8d90f17cc568bb3a6f6319796f89086543f75d92
Corey Rowe <coreyr1196@gmail.com>
2021-12-23 13:57:07 -0500

    Replace cat with awk to avoid newline issues

--- merge_gtfs.sh
+++ merge_gtfs.sh
@@ -19,22 +19,22 @@ mkdir gtfs_merged/
 # Remove the header line from each file in the second GTFS archive.
 find gtfs_two/ -type f -exec sed -i '1d' {} \;
 
-cat gtfs_one/agency.txt gtfs_two/agency.txt > gtfs_merged/agency.txt
-cat gtfs_one/calendar_dates.txt gtfs_two/calendar_dates.txt > gtfs_merged/calendar_dates.txt
-cat gtfs_one/calendar.txt gtfs_two/calendar.txt > gtfs_merged/calendar.txt
-cat gtfs_one/directions.txt gtfs_two/directions.txt > gtfs_merged/directions.txt
-cat gtfs_one/fare_attributes.txt gtfs_two/fare_attributes.txt > gtfs_merged/fare_attributes.txt
-cat gtfs_one/fare_rules.txt gtfs_two/fare_rules.txt > gtfs_merged/fare_rules.txt
-cat gtfs_one/feed_info.txt gtfs_two/feed_info.txt > gtfs_merged/feed_info.txt
-cat gtfs_one/frequencies.txt gtfs_two/frequencies.txt > gtfs_merged/frequencies.txt
-cat gtfs_one/routes.txt gtfs_two/routes.txt > gtfs_merged/routes.txt
-cat gtfs_one/shapes.txt gtfs_two/shapes.txt > gtfs_merged/shapes.txt
-cat gtfs_one/stops.txt gtfs_two/stops.txt > gtfs_merged/stops.txt
-cat gtfs_one/stop_times.txt gtfs_two/stop_times.txt > gtfs_merged/stop_times.txt
-cat gtfs_one/timepoints.txt gtfs_two/timepoints.txt > gtfs_merged/timepoints.txt
-cat gtfs_one/timepoints_times.txt gtfs_two/timepoints_times.txt > gtfs_merged/timepoints_times.txt
-cat gtfs_one/transfers.txt gtfs_two/transfers.txt > gtfs_merged/transfers.txt
-cat gtfs_one/trips.txt gtfs_two/trips.txt > gtfs_merged/trips.txt
+awk 1 gtfs_one/agency.txt gtfs_two/agency.txt > gtfs_merged/agency.txt
+awk 1 gtfs_one/calendar_dates.txt gtfs_two/calendar_dates.txt > gtfs_merged/calendar_dates.txt
+awk 1 gtfs_one/calendar.txt gtfs_two/calendar.txt > gtfs_merged/calendar.txt
+awk 1 gtfs_one/directions.txt gtfs_two/directions.txt > gtfs_merged/directions.txt
+awk 1 gtfs_one/fare_attributes.txt gtfs_two/fare_attributes.txt > gtfs_merged/fare_attributes.txt
+awk 1 gtfs_one/fare_rules.txt gtfs_two/fare_rules.txt > gtfs_merged/fare_rules.txt
+awk 1 gtfs_one/feed_info.txt gtfs_two/feed_info.txt > gtfs_merged/feed_info.txt
+awk 1 gtfs_one/frequencies.txt gtfs_two/frequencies.txt > gtfs_merged/frequencies.txt
+awk 1 gtfs_one/routes.txt gtfs_two/routes.txt > gtfs_merged/routes.txt
+awk 1 gtfs_one/shapes.txt gtfs_two/shapes.txt > gtfs_merged/shapes.txt
+awk 1 gtfs_one/stops.txt gtfs_two/stops.txt > gtfs_merged/stops.txt
+awk 1 gtfs_one/stop_times.txt gtfs_two/stop_times.txt > gtfs_merged/stop_times.txt
+awk 1 gtfs_one/timepoints.txt gtfs_two/timepoints.txt > gtfs_merged/timepoints.txt
+awk 1 gtfs_one/timepoints_times.txt gtfs_two/timepoints_times.txt > gtfs_merged/timepoints_times.txt
+awk 1 gtfs_one/transfers.txt gtfs_two/transfers.txt > gtfs_merged/transfers.txt
+awk 1 gtfs_one/trips.txt gtfs_two/trips.txt > gtfs_merged/trips.txt
 
 cd gtfs_merged/
 
